fa4a34bada1c077abd530d853854b088cc8d510f
masterinfo <wilfrid_mo@hotmail.com>
2020-11-29 19:52:08 +0100

    test python github 6

--- app.py
+++ app.py
@@ -18,6 +18,9 @@ def hello_world3():
     return 'Hello World!  qui declenche jenkins depui github'
 
 
+@app.route('/git3')
+def hello_world32():
+    return 'Hello World!  qui declenche jenkins depui github3'
 
 if __name__ == '__main__':
     app.run()
