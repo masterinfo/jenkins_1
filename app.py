c93014a9e0562c5a3861330948101883cb8a2770
masterinfo <wilfrid_mo@hotmail.com>
2020-11-29 19:33:03 +0100

    test python github 5

--- app.py
+++ app.py
@@ -14,8 +14,6 @@ def hello_world2():
     return 'Hello World! vive github de wilfrid'
 
 
-#bonjour
-
 @app.route('/git')
 def hello_world3():
     return 'Hello World!  qui declenche jenkins depui github'
