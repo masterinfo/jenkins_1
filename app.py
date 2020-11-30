1ea0c663b576a1f3988dd47fe9b6911a95767821
masterinfo <wilfrid_mo@hotmail.com>
2020-11-30 12:00:36 +0100

    masterinfo

--- app.py
+++ app.py
@@ -8,7 +8,9 @@ def hello_world():
     return 'Hello World! vive github'
 
 
-
+@app.route('/gpizza')
+def hello_world3():
+    return 'Hello World!  qui declenche jenkins depui github pizza'
 
 @app.route('/git')
 def hello_world3():
