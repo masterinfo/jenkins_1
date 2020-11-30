d9893cd094c29875f73bebac01c687b57957eaf2
masterinfo <wilfrid_mo@hotmail.com>
2020-11-30 10:22:25 +0100

    test python github scm poll

--- app.py
+++ app.py
@@ -13,7 +13,14 @@ def hello_world2():
     return 'Hello World! vive github de wilfrid'
 
 
+@app.route('/git')
+def hello_world3():
+    return 'Hello World!  qui declenche jenkins depui github'
 
+
+@app.route('/wildfdsf')
+def hello_world2dfsd():
+    return 'Hello World! vive github de wilfrid'
 
 
 if __name__ == '__main__':
